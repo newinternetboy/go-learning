39ba13ddd64a3fab86fe5e4a31927d9ce021b511
newinternetboy <961937794@qq.com>
2019-06-20 14:57:53 +0800

    func_callback

--- example/function/func_callback.go
+++ example/function/func_callback.go
@@ -2,13 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
+type Callback func(x, y int) int
 
+func main() {
+	x, y := 1, 2
+	fmt.Println(test(x, y, add))
 }
 
-func add(x, y int){
+func add(x, y int) int {
 	return x + y
 }
 
 //定义回调函数
-func
+func test(x, y int, callback Callback) int {
+	return callback(x, y)
+}
